431b886f40811fd2a98fd54a5c06f993ef6edcbf
mxiiii <mxiiii@users.noreply.github.com>
2015-12-10 15:38:28 +0100

    Create hotfix.sh

---
+++ hotfix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# The perfect rootserver UPDATE
+# by mxiiii
+# https://github.com/mxiiii/perfect_update
+# Thanks to https://github.com/zypr/perfectrootserver
+# and https://github.com/andryyy/mailcow
+# Compatible with Debian 8.x (jessie)
+#
+# Hotfix v1.0
+#
+install -m 755 ~/sources/update/mc_setup_relayhost /usr/local/sbin/
+cp ~/sources/update/functions.inc.php /var/www/mail/inc/ 
